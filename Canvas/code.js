db8ad672cce97222f690e201128c031b4f797801
abbshill <144380708+abbshill@users.noreply.github.com>
2023-10-20 22:26:26 -0500

    Add files via upload

---
+++ Canvas/code.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById("c1")
+let painter = canvas.getContext("2d")
+painter.fillStyle = "#FF0000"
+painter.fillRect(200, 100, 50, 50);
+painter.fillRect(300,100,50,50);
+
+
+// let a = 0
+// while(a<5) {
+//     alert("Hello!")
+//     a = a + 1
+// }
